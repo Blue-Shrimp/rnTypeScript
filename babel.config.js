571048ae8483296baef8bffaa3dbb707c4abb42e
Blue-Shrimp <wkdtmd1202@naver.com>
2023-06-02 13:43:04 +0900

    feat: 프로젝트 구조 설정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  plugins: [
+    [
+      'module-resolver',
+      {
+        global: ['./'],
+        extensions: ['.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@root': './',
+          '@common': './src/common',
+          '@stores': './src/store',
+          '@network': './src/network',
+          '@screens': './src/screen',
+          '@components': './src/components',
+          '@images': './assets/images',
+        },
+      },
+    ],
+  ],
+}
